205c36903c69c65a7df091f7b6cb8b4b898d4ec1
lyz-vue-springboot <2736836265@qq.com>
2023-09-15 21:19:52 +0800

    api

--- cloud-bcyy-feign-api/src/main/java/com/bcyy/apis/webSocket/WebSocketApi.java
+++ cloud-bcyy-feign-api/src/main/java/com/bcyy/apis/webSocket/WebSocketApi.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(value = "bcyy-webSocket",configuration = FeignConfig.class)
 public interface WebSocketApi {
+    //实时通信
     @GetMapping("/webSocker/push")
     void push(@RequestParam("message")String message, @RequestParam("uId")String uId);
 }
